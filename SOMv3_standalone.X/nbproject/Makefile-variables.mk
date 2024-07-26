af16241cd8677fe6344847b42ddda33782d419ed
Marc Fortier <mfortier@elabore.ca>
2024-07-25 21:18:43 -0400

    Ajout projet MPLabX et gerbers SmartSom et SOM standalone

---
+++ SOMv3_standalone.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SOMv3_standalone.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SOMv3_standalone.X.production.hex
