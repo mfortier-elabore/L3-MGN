3ee609aa626bed6d317f4f1191f83fc974ec2783
Marc Fortier <mfortier@elabore.ca>
2024-08-23 16:37:42 -0400

    Fichiers tempo le temps de convertir

---
+++ SOMv3_standalone.X/tdd/LIS2DTW12/LIS2DTW12Test.c
@@ -0,0 +1,17 @@
+#include "../unity/unity_fixture.h"
+#include "../../LIS2DTW12.h"
+
+TEST_GROUP(LIS2DTW12);
+
+TEST_SETUP(LIS2DTW12)
+{
+}
+
+TEST_TEAR_DOWN(LIS2DTW12)
+{
+}
+
+TEST(LIS2DTW12, TemplateTest)
+{
+	TEST_FAIL_MESSAGE("Template test");
+}
---
+++ SOMv3_standalone.X/tdd/LIS2DTW12/LIS2DTW12TestRunner.c
@@ -0,0 +1,7 @@
+#include "../unity/unity_fixture.h"
+
+TEST_GROUP_RUNNER(LIS2DTW12)
+{
+    RUN_TEST_CASE(LIS2DTW12, StartHere);
+}
+
